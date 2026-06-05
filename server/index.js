8845acb8bdb50e539f0e937bb1aadc75cf2047a2
Shubham Sura <shubhamsura1998@gmail.com>
2026-06-05 11:21:05 +0530

    added node-server and links ui

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from SignBridge server!" });
+  });
+  
+app.listen(PORT, () => {
+  console.log(`SignBridge Server listening on ${PORT}`);
+});
